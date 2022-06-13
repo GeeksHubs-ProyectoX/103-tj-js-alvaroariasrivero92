e9cf15420fc73173ade21e24320497f29043a87f
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2022-06-13 23:54:54 +0200

    tests passed

--- test/test01.js
+++ test/test01.js
@@ -1,4 +1,16 @@
 function tiers(index) {
-	return "";
+	let string = "";
+	for (let i = 1; i <= index; i++) {
+  		// printing spaces
+  		for (let j = 0; j < index - i; j++) {
+    		string += " ";
+  		}
+  	// printing star
+  	for (let k = 0; k < i; k++) {
+    string += "*";
+  	}
+  	string += "\n";
+	}
+	return string;
 }
 module.exports = tiers;
